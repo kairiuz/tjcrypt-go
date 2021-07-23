af39471bae556f05c2b10e3675e63870cedc4f32
wobm <wobm@teknik.io>
2021-07-23 09:52:12 +0700

    remove unused consts

--- cmd/tjdecrypt/tjdecrypt.go
+++ cmd/tjdecrypt/tjdecrypt.go
@@ -8,9 +8,6 @@ import (
 	"git.teknik.io/wobm/tjcrypt-go/pkg/tjcrypt"
 )
 
-const TJ_DEFAULT_KEY = "\xd2\"\x82\x7f\xe9\xd3r\xa5$\x90\x8cm\n\x96\xcb\xa3"
-const TJ_PASSWORD = "MODDEDBYPL0NK!!!"
-
 func check(e error) {
 	if e != nil {
 		panic(e)
